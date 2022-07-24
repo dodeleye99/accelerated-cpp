1b7c2a63d63252c101086801cc6e9c40d60cac83
babs099 <109389802+babs099@users.noreply.github.com>
2022-07-24 20:53:37 +0100

    Create hello_world_sample.cpp

---
+++ 0_HelloWorld/hello_world_sample.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// // a small C++ program
+// #include <iostream>
+
+// int main()
+// {
+//     std::cout << "Hello, world!" << std::endl;
+//     return 0;
+// }
+
+
+int main()
+{
+    // Output lines of code that will form a Hello World program
+    std::cout << "// a small C++ program" << std::endl;
+    std::cout << "#include <iostream>" << std::endl;
+    std::cout << "\nint main()\n{" << std::endl;
+    std::cout << "\tstd::cout << \"Hello, world!\" << std::endl;" << std::endl;
+    std::cout << "\treturn 0;\n}" << std::endl;
+}
