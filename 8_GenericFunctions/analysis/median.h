0e7de5c1f7aa0b7df8e1c083279bfc00f6add029
babs099 <109389802+babs099@users.noreply.github.com>
2022-08-04 19:17:06 +0100

    Update median.h

--- 8_GenericFunctions/analysis/median.h
+++ 8_GenericFunctions/analysis/median.h
@@ -9,33 +9,30 @@
 #include <vector>       // defines std::domain_error
 #include <iostream>
 
+
 /**
- * Calculates the median of the (assumed numerical) values in a sequence defined by 
+ * Calculates the median of the (assumed numerical type T) values in a sequence defined by 
  * the range of iterators [b, e)
  * 
  * Iterator required: random access iterator 
  **/
-template<class Ran> 
+template<class T, class Ran> 
 double median(Ran b, Ran e)
 {
-    /**
-     * (The ptrdiff_t type is used to define differences between pointers.)
-     * 
-     * Get the size of the sequence by calculating the distance between the iterators.
-     * For random access iterators, it can be found using the subtration operator.
-     **/
-    const std::ptrdiff_t size = e - b;
 
-    // throw an error if the size is zero (empty sequence)
-    if (size == 0) {
+    // throw an error if the iterators are at the same position (empty sequence)
+    if (b == e) {
         throw std::domain_error("median of an empty container");
     }
 
-    // use the size to calculate the midpoint of the sequence.
-    std::ptrdiff_t mid  = size / 2;
-
     // use a copy of the sequence to prevent the original from being modified.
-    std::vector<double> v(b, e);
+    std::vector<T> v(b, e);
+    
+    // get the size of the sequence by calculating the distance between the iterators.
+    std::size_t size = v.size();
+
+    // use the size to calculate the midpoint of the sequence.
+    std::size_t mid  = size / 2;
     
     // ensure the sequence is sorted to ensure a correct result.
     std::sort(v.begin(), v.end());
