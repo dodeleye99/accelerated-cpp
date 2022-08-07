a450d7ac7e63da1ca894ff96e693f609a630e87b
babs099 <109389802+babs099@users.noreply.github.com>
2022-08-07 01:45:00 +0100

    Update pointer_test.cpp

--- 10_Pointers&Arrays/pointer_test.cpp
+++ 10_Pointers&Arrays/pointer_test.cpp
@@ -23,7 +23,7 @@ int main()
  * ==========================
  * Declaring pointers:
  * 
- * typename * p_name
+ * typename * p_name;
  * --------------------------
  * A pointer can be set to the adress of an object of corresponding type,
  * or to another pointer of the same type (including the initial pointer of an array - simply assign it to an array variable)
