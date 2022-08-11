8b6da942f030b8f39a58eee287d7aea1dd72fc6c
babs099 <109389802+babs099@users.noreply.github.com>
2022-08-11 22:11:21 +0100

    Update Vec.h

--- 11_TemplateClasses/vec/Vec.h
+++ 11_TemplateClasses/vec/Vec.h
@@ -31,6 +31,7 @@ public:
     typedef const T& const_reference;           // constant reference type used for read-only access
 
     // --------------- Constructors ---------------
+    
     // default constructor
     Vec() { create(); } 
     // copy constructor                                
@@ -135,10 +136,149 @@ private:
     // -------- Misc. -------
 
     // outputs the number of allocated (but not necessary initialised) memory spaces for the Vec elements.
-    size_type size_limit() const { return limit - data; }
+    difference_type size_limit() const { return limit - data; }
 };
 
 #endif
+
+// === Implementation of the undefined class methods ===
+
+template <class T> 
+Vec<T>& Vec<T>::operator=(const Vec& rhs) 
+{
+    // check for self-assignment, in which case do nothing.
+    if(&rhs != this) {
+
+        // free the array in the left hand side
+        uncreate();
+
+        // copy elements from the right-hand side to the left-hand side (this object)
+        create(rhs.begin(), rhs.end());
+    }
+    // ensure to return the modified object.
+    return *this;
+}
+
+template <class T> void Vec<T>::create()
+{   
+    // initialise the Vec as empty by simply setting all the iterators to 0.
+    data = avail = limit = 0;
+}
+
+template <class T> void Vec<T>::create(size_type n, const T& val)
+{
+    // allocate enough memory for n elements (of type T), setting data to the returned pointer towards the first position
+    data = alloc.allocate(n);
+    // no extra memory included yet, so have both avail and limit point to the end
+    limit = avail = data + n;
+    // initialise the elements as copies of val
+    std::uninitialized_fill(data, limit, val);
+}
+
+template <class T> void Vec<T>::create(const_iterator i, const_iterator j)
+{   
+    /**
+     * Allocate enough memory for elements of quantity equal to the distance between the iterators
+     * (giving the number of elements in the original sequence). Then set the data member to the returned
+     * pointer towards the first allocated position.
+     **/
+    data = alloc.allocate(j - i);
+    /**
+     * Initalise the elements as copies of those between the iterators [i, j), 
+     * adding them to the dynamic array indicated by data 
+     * (returns one past the last element initialised)
+     **/
+    limit = avail = std::uninitialized_copy(i, j, data);
+}
+
+template <class T> void Vec<T>::uncreate()
+{   
+    // we only need to uninitialise if data is not already 0 (indicating an empty Vec)
+    if(data) {
+        // destroy (in reverse order) the elements that were constructed
+        iterator it = avail;
+        while (it != data)
+            alloc.destroy(--it);
+        
+        // return (free) all the space that was allocated for the Vec elements
+        alloc.deallocate(data, size_limit());
+        
+    }
+    // reset pointers to indicate that the Vec is empty again
+    data = limit = avail = 0;
+}
+
+template <class T> void Vec<T>::grow()
+{
+    /**
+     * While "growing", allocate twice as much space as currently in use
+     * (if empty, then allocate only a single space)
+     **/
+    size_type new_size = std::max(2 * size_limit(), ptrdiff_t(1));
+
+    // allocate new space and copy existing elements to the new space
+    iterator new_data = alloc.allocate(new_size);
+    iterator new_avail = std::uninitialized_copy(data, avail, new_data);
+
+    // return (free) the old space
+    uncreate();
+
+    // set the iterators to point to the newly allocated space
+    data = new_data;
+    avail = new_avail;
+    limit = data + new_size;
+}
+
+template <class T> void Vec<T>::unchecked_append(const T& val)
+{
+    /**
+     * At the end of the sequence of constructed elements, initialise a single element as a copy of val.
+     * Then increment avail to point to the next free (pre-allocated), constructable space.  
+     **/
+    alloc.construct(avail++, val);
+}
+
+/**
+ * ==========================================================
+ * ALLOCATOR OBJECTS AND THEIR USEFULNESS
+ * ==========================================================
+ * An std::allocator<T> object is used to manage memory at a very low level during runtime.
+ * 
+ * It is used to allocate memory for an object (or an array of objects) of type T to utilise, but WITHOUT 
+ * also constructing/initialising an object at that memory location. This has to be done separately.
+ * It may also be used to destroy an object at a memory location, and deallocate (free) the memory, 
+ * both of which must also be done separately.
+ * 
+ * This is in contrast with allocating/deallocating memory through the 'new' and 'delete' operators,
+ * which do these in a single step.
+ *
+ * For our Vec class which utilises a dynamic array to hold the elements, the allocator object allows
+ * us to only construct elements as they are added to the Vec. For memory locations in the array yet
+ * to be used, we can construct them later if more elements need to be added.
+ * 
+ * Otherwise, with the 'new' operator, each part of the array would be constructed twice: 
+ * once immediately after allocation, and again when we actually want to use the position to add an
+ * element to the Vec. This is not only inefficient, but redundant.
+ * 
+ * Therefore allocator objects not only provide a greater degree of flexibility in regards to dynamic memory
+ * management, but also more efficiency in terms of performance. 
+ * 
+ **/
+
+/**
+ * =====================================
+ * MEMORY FUCTIONS FROM STANDARD LIBRARY
+ * =====================================
+ * uninitialized_fill(b, e, x): initialises a yet-to-be constructed sequence (usually a dynamic array)
+ *                              bound by iterators [b, e) pointing to the memory locations already 
+ *                              allocated for the sequence, assigning each of their values to the 
+ *                              value of x.
+ * 
+ * uninitialized_copy(b, e, d): copies a sequence bound by iterators [b, e) into an already allocated
+ *                              memory locations starting from the position indicated by iterator d, 
+ *                              initialising a copy of the original sequence, and returns an iterator 
+ *                              to the last element of this new sequence.                           
+ **/
 
 /**
  * =================================================
@@ -187,31 +327,4 @@ private:
  * is initialisation of one Vec<double> object, using the default constructior,
  * followed by applying THE assignment operator on that object to another (temporary) object newly initialised by a constructor
  * taking a single integer. It would be more efficient to simply write Vec<double> v1(10);
- **/
-
-/**
- * ==========================================================
- * ALLOCATOR OBJECTS AND THEIR USEFULNESS
- * ==========================================================
- * An std::allocator<T> object is used to manage memory at a very low level during runtime.
- * 
- * It is used to allocate memory for an object (or an array of objects) of type T to utilise, but WITHOUT 
- * also constructing/initialising an object at that memory location. This has to be done separately.
- * It may also be used to destroy an object at a memory location, and deallocate (free) the memory, 
- * both of which must also be done separately.
- * 
- * This is in contrast with allocating/deallocating memory through the 'new' and 'delete' operators,
- * which do these in a single step.
- *
- * For our Vec class which utilises a dynamic array to hold the elements, the allocator object allows
- * us to only construct elements as they are added to the Vec. For memory locations in the array yet
- * to be used, we can construct them later if more elements need to be added.
- * 
- * Otherwise, with the 'new' operator, each part of the array would be constructed twice: 
- * once immediately after allocation, and again when we actually want to use the position to add an
- * element to the Vec. This is not only inefficient, but redundant.
- * 
- * Therefore allocator objects not only provide a greater degree of flexibility in regards to dynamic memory
- * management, but also more efficiency in terms of performance. 
- * 
  **/
