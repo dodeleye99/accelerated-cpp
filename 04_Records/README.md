8cb55820ac96ae8cfb763dced63d90c945f87c2d
babs099 <109389802+babs099@users.noreply.github.com>
2022-09-28 01:13:29 +0100

    Update squares program
    Rather than outputting an extremely long list of square numbers,
    the user now has the choice to decide how far they want the list
    to go.

--- 04_Records/README.md
+++ 04_Records/README.md
@@ -11,7 +11,7 @@ The input grades are now restricted to integers rather than decimals, to prevent
 2) [`records2`](records2) breaks the source code down into multiple files to make it more organised and easier to find particular code. The source code is now located in a new subdirectory called `src`. 
 
 ### Extension:
-3) [`squares`](squares) does further experimentation with neatly writting a table of results. It computes the squares of 0 to 999.
+3) [`squares`](squares) does further experimentation with neatly writting a table of results. It computes the squares of 0 to any given positive integer.
 
 ### New Concepts:
 * defining `struct` types, used to group data values to form a data structure, so that it can be treated as a single entity.
@@ -19,3 +19,5 @@ The input grades are now restricted to integers rather than decimals, to prevent
 * sorting struct types by specifying the comparison function used to define the ordering.
 * modular programming, breaking down source code into separate components.
 * header files, containing name declarations of data values and types, such as functions and structures, to be defined in a separate, corresponding C++ source code file.
+* using basic maths operatons from the `<cmath>` standard header. 
+* the `break` statement, used to terminate the current loop.
--- 04_Records/squares/main.cpp
+++ 04_Records/squares/main.cpp
@@ -1,14 +1,21 @@
-// calculate the squares of 0 to 999
-#include <iostream> // defines cout, endl
+// calculate the squares of 0 to a given number
+
+
+#include <cmath>    // defines sqrt, pow
+#include <iostream> // defines cin, cout, endl
 #include <iomanip>  // defines setw
 #include <string>   // defines string
 #include <ios>      // defines streamsize
 
+using std::cin;
 using std::cout;
 using std::endl;
+using std::pow;
 using std::setw;
+using std::sqrt;
 using std::string;
 using std::streamsize;
+
 
 // calculates the number of digits of a given integer
 streamsize get_digits(int n)
@@ -20,7 +27,7 @@ streamsize get_digits(int n)
      * Thus get_digits(2567) = 4.
     **/
 
-    // The special case, zero, has one digit
+    // the special case, zero, has one digit
     if(n == 0) {
         return 1;
     }
@@ -36,20 +43,66 @@ streamsize get_digits(int n)
 
 
 int main()
-{
+{   
+    // to hold the cap for the sequence of numbers to calculate squares from
+    int limit;
+
+    // introduce the program to the user.
+    cout << "This program lists the square numbers of consecutive integers "
+            "starting from 0." << endl;
+
+    // prompt the user to enter the final number to calculate squares from.
+    cout << "Which number would you like the consecutive integers to end at?"
+        << endl;
+
+    // feed the user's input into the variable.
+    cin >> limit;
+
+    // complian and exit if the input stream had problems reading the input.
+    if(!cin) {
+        // interpret this as the user failing to enter an integer.
+        cout << "Invalid input. You must enter a whole number." << endl; 
+        return -1;
+    }
+    
+    // complain and exit if the user entered a negative number.
+    if(limit < 0) {
+        cout << "Invalid input. The integer must be positive." << endl; 
+        return -1;
+    }
+
     /**
      * Use get_digits() to find out how many digits are in 999 (and its square).
-     * This will help us know the padding we need to neatly output the table of values.
+     * This will help us know the padding we need to neatly output the table of
+     * values.
     */
-    int count = 999;
-    streamsize max_digits = get_digits(count);
+    streamsize max_digits = get_digits(limit);
 
-    cout << "The squares of integers from 0 to " << count << ":" << endl;
-    for(int i = 0; i != count + 1; ++i) {
+    // prepare to list the squares.
+    cout << "\nThe squares of integers from 0 to " << limit << ":" << endl;
 
-        int square = i * i;
+    for(int i = 0; i != limit + 1; ++i) {
+        
+        // end the loop early if INT_MAX will be surpassed 
+        // (to prevent overflows).
+        if(i > sqrt(INT_MAX)) {
+            cout << "Stopping early, squares are getting too big." 
+                << endl;
+            // (use the break statement to exit the loop entirely.)
+            break;
+        }
+        /**
+         * Technically, larger numbers can be calculated, by using, 
+         * instead of the int type, a type that can store even larger 
+         * numbers, such as long.
+         **/
+
+        // calculate the square of the current number traversed over.
+        int square = pow(i, 2);
+
         // find an apporiate padding.
         streamsize pad = max_digits - get_digits(i) + 1 + get_digits(square);
+
         // use setw() to write blank space of a given length.
         // alternatively, initialise a string of blank space.
         cout << i << setw(pad) << square << endl;
